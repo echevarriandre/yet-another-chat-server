e9c913e6bfa9411d614db233179be3f6bb2ea7be
André Echevarria <echevarriandre@gmail.com>
2022-09-03 21:37:21 +0100

    feat: add types, update user handling, ask user...

--- index.ts
+++ index.ts
@@ -1,50 +1,42 @@
 import { Server } from "socket.io";
+import {
+  ClientServerEventNames as CS,
+  ClientToServerEvents,
+  ServerClientEventNames as SC,
+  ServerToClientEvents,
+} from "./@types";
+import { Users } from "./models/Users";
 
-interface ServerToClientEvents {
-  disconnect: () => void;
-  "chat-new-user": (user: User) => void;
-}
+const io = new Server<ClientToServerEvents, ServerToClientEvents>(5000, { cors: { origin: "*" } });
 
-interface ClientToServerEvents {
-  "chat-connect": (username: string) => void;
-  "chat-fetch-users": (callback: (users: User[]) => void) => void;
-}
-
-interface User {
-  id: string;
-  username: string;
-}
-
-const io = new Server<ClientToServerEvents, ServerToClientEvents>(5000, {
-  cors: {
-    origin: "*",
-  },
-});
-
-const users = new Map();
-
-function getUsers(filterOutId: string = ""): User[] {
-  const usersJson: User[] = [];
-  users.forEach((username, id) => {
-    if (id !== filterOutId) usersJson.push({ id: id, username: username });
-  });
-  return usersJson;
-}
+const activeUsers = new Users();
 
 io.on("connection", (socket) => {
   console.log(socket.id, "connected");
 
-  socket.on("chat-connect", (username: string) => {
-    users.set(socket.id, username);
-    socket.broadcast.emit("chat-new-user", { id: socket.id, username });
+  socket.on(CS.ChatConnect, (username: string) => {
+    activeUsers.addUser(socket.id, username);
+    socket.broadcast.emit(SC.ChatNewUser, { id: socket.id, username, chatting: false });
   });
 
-  socket.on("chat-fetch-users", (callback) => {
-    callback(getUsers(socket.id));
+  socket.on(CS.ChatFetchUsers, (callback) => {
+    callback(activeUsers.getUsers(socket.id));
+  });
+
+  socket.on(CS.ChatAskUser, (id: string) => {
+    const username = activeUsers.getUsernameById(id);
+    const currentUser = activeUsers.getUsernameById(socket.id);
+    if (!username) {
+      socket.emit(SC.ChatAskUserNotFound, "Username not found");
+      return;
+    }
+
+    socket.to(id).emit(SC.ChatUserRequest, currentUser!);
   });
 
   socket.on("disconnect", () => {
     console.log(socket.id, "disconnected");
-    users.delete(socket.id);
+    activeUsers.deleteUser(socket.id);
+    socket.broadcast.emit(SC.ChatRemoveUser, socket.id);
   });
 });
